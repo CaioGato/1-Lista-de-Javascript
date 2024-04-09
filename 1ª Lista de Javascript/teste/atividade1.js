470b645166801dd65fe791b22fd88ccef5d223b0
CaioBorba <160753615+CaioGato@users.noreply.github.com>
2024-04-09 11:12:49 -0300

    Add files via upload

---
+++ 1ª Lista de Javascript/teste/atividade1.js
@@ -0,0 +1,8 @@
+function somarNumeros(){
+    let soma = 0;
+    let continuar = true;
+    while (continuar) {
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar):"))
+        
+    }
+} 
